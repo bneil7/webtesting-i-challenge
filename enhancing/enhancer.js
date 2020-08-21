639e3d1a164d59a420c540d931af3028d8d8fd9a
Brandon Neil <63384257+bneil7@users.noreply.github.com>
2020-08-20 19:28:57 -0600

    all tests pass and fail when expected, project meets mvp now

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,41 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  const enhancedItem = { ...item };
+
+  if (enhancedItem.enhancement === 20) {
+    return enhancedItem;
+  } else {
+    enhancedItem.enhancement += 1;
+    return enhancedItem;
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  const failedItem = { ...item };
+
+  // If the item's enhancement is less than 15, the durability of the item is decreased by 5.
+  // If the item's enhancement level is greater than 16, the enhancement level decreases by 1.
+  // If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
+
+  if (failedItem.enhancement >= 15) {
+    if (failedItem.enhancement > 16) {
+      failedItem.durability -= 10;
+      failedItem.enhancement -= 1;
+    } else {
+      failedItem.durability -= 10;
+    }
+  } else if (failedItem.enhancement < 15) {
+    failedItem.durability -= 5;
+  }
+
+  return failedItem;
 }
 
 function repair(item) {
-  return { ...item };
+  const repairedItem = { ...item };
+  repairedItem.durability = 100;
+  return repairedItem;
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,48 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+describe("enhancer", () => {
+  describe("success(item)", () => {
+    it("should increase item enhancement by 1, only if item is < 20", () => {
+      expect(
+        enhancer.success({ name: "helmet", durability: 100, enhancement: 10 })
+      ).toEqual({ name: "helmet", durability: 100, enhancement: 11 });
+
+      expect(
+        enhancer.success({ name: "helmet", durability: 100, enhancement: 20 })
+      ).toEqual({ name: "helmet", durability: 100, enhancement: 20 });
+
+      expect(
+        enhancer.success({ name: "helmet", durability: 69, enhancement: 18 })
+      ).toEqual({ name: "helmet", durability: 69, enhancement: 19 });
+    });
+  });
+
+  describe("fail(item)", () => {
+    it("decreases durability by 5 (if enhancement level < 15) & decreases durability by 10 (if enh lv >= 15) & decreases enh lv by 1 (if enh lv > 16)", () => {
+      expect(
+        enhancer.fail({ name: "sword", durability: 50, enhancement: 0 })
+      ).toEqual({ name: "sword", durability: 45, enhancement: 0 });
+
+      expect(
+        enhancer.fail({ name: "sword", durability: 50, enhancement: 15 })
+      ).toEqual({ name: "sword", durability: 40, enhancement: 15 });
+
+      expect(
+        enhancer.fail({ name: "sword", durability: 50, enhancement: 17 })
+      ).toEqual({ name: "sword", durability: 40, enhancement: 16 });
+    });
+  });
+
+  describe("repair(item)", () => {
+    it("returns new item with durability restored to 100", () => {
+      expect(
+        enhancer.repair({ name: "armor", durability: 50, enhancement: 0 })
+      ).toEqual({ name: "armor", durability: 100, enhancement: 0 });
+    });
+  });
+
+  describe("get()", () => {
+    // stretch
+  });
+});
